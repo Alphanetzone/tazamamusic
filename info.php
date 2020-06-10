df4f80cd64f023d50540c71d7fc7a5108d569e07
Dauli <alexisangedauli@gmail.com>
2020-06-10 18:53:36 +0200

    Initial Commit

---
+++ info.php
@@ -0,0 +1,13 @@
+<?php
+// Software Name
+$name = 'alphanetzone';
+
+// Software Author
+$author = 'phpSound';
+
+// Software URL
+$url = 'https://alphanetzone.co.za';
+
+// Software Version
+$version = '4.2.0';
+?>
